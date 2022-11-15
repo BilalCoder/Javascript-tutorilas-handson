d0d946ba1875fe1264b372a241096c055831755e
Bilal Khan <bilal.khan@publicissapient.com>
2022-11-15 19:00:30 +0530

    Starter commit

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 // if (a === "Bilal") {
 // 	alert("Bilal is fun");
 // }
-48 +  9 + 8;
-console.log(48 +  9 + 8);
+// 48 +  9 + 8;
+// console.log(48 +  9 + 8);
 
-let variable = 10000000000000;
-let variable2 = 10000000000000;
-let variable3 = 3.5;
-console.log(variable);
-if (variable === variable) {
-	alert("Value = " + (variable + variable2) + " typeof - a - " + typeof(variable) + 
-  " b - " + typeof(variable2) + 
-  " c - " + typeof(variable3));
-}
+// let variable = 10000000000000;
+// let variable2 = 10000000000000;
+// let variable3 = 3.5;
+// console.log(variable);
+// if (variable === variable) {
+// 	alert("Value = " + (variable + variable2) + " typeof - a - " + typeof(variable) + 
+//   " b - " + typeof(variable2) + 
+//   " c - " + typeof(variable3));
+// }
+
+let a = 10;
+//let a = 30; //error
+
+a = 20;
+
+var b = 20;
+var b = 21;  //no error for redeclaring in var
+
+const PI = 3.14;
+PI = 2;
+console.log(PI);  // error - Uncaught TypeError: Assignment to constant variable

bd84c763b4b61acc9a663d7025875157fdd5519b
Bilal Khan <bilal.khan@publicissapient.com>
2022-11-15 17:08:00 +0530

    Starter commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let a = "Bilal";
+// if (a === "Bilal") {
+// 	alert("Bilal is fun");
+// }
+48 +  9 + 8;
+console.log(48 +  9 + 8);
+
+let variable = 10000000000000;
+let variable2 = 10000000000000;
+let variable3 = 3.5;
+console.log(variable);
+if (variable === variable) {
+	alert("Value = " + (variable + variable2) + " typeof - a - " + typeof(variable) + 
+  " b - " + typeof(variable2) + 
+  " c - " + typeof(variable3));
+}
